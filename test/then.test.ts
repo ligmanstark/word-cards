6879973b2fa4efba2579c1ae52cf84a15159b0aa
levis <miduwaystark@gmail.com>
2024-02-10 19:36:14 +0400

    modified:   test/then.test.ts

--- test/then.test.ts
+++ test/then.test.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BASE_URL, EN_RU, SEARCH_WORD } from '../src/utils/consts';
 
-const api =process.env.REACT_APP_API_TEST_KEY
+const api = process.env.REACT_APP_API_TEST_KEY;
 
 const FULL_URL = BASE_URL + api + EN_RU + SEARCH_WORD;
 
@@ -10,5 +10,3 @@ test('get 200', () => {
     expect(data.data.def[0].text).toMatch('bar');
   });
 });
-
-
